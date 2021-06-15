b183189dd346c072b32775bb9fd1123ccaf29bc5
hsa-98 <harshsanjayagrawal89@gmail.com>
2021-06-16 01:46:33 +0530

    Can use console to delete a person

--- day9/src/PhoneBook.java
+++ day9/src/PhoneBook.java
@@ -17,7 +17,9 @@ public class PhoneBook {
         Contact num1 = new Contact();
         num1.Contact(num);
         editContact(num1);
+        deleteContact(num1);
         return;
+
         }
 
     /**
@@ -48,6 +50,27 @@ public class PhoneBook {
             System.out.println(num1.phonebook);
         }
         else {
+            System.out.println("Enter a valid key");
+        }
+        return;
+    }
+
+    /**
+     * deleteContact:
+     * @param num1
+     * This function deletes the contact input by the user.
+     */
+    public static void  deleteContact(Contact num1){
+        System.out.println("Enter the contact name you want to delete");
+        Scanner sc = new Scanner(System.in);
+        String key = sc.next();
+        boolean isKey = num1.phonebook.containsKey(key); //Checks if the key exists or not
+        if(isKey){
+            num1.phonebook.remove(key); //removes a key from hashmap.
+            System.out.println("Key is deleted");
+            System.out.println(num1.phonebook);
+        }
+        else{
             System.out.println("Enter a valid key");
         }
         return;
